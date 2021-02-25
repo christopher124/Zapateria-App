aa10fdd2fedcc6d4ae0dcf57c67e004830fc550b
christopher124 <urri_@hotmail,com>
2021-02-24 23:29:34 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {InicioComponent} from "../app/components/inicio/inicio.component"
+import {NosotrosComponent} from "../app/components/nosotros/nosotros.component"
+import {NoticiasComponent} from "../app/components/noticias/noticias.component"
+import {LoginComponent} from "../app/components/user/login/login.component"
+
+const routes: Routes = [
+
+  {path: 'inicio', component: InicioComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'noticias', component: NoticiasComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import {LoginComponent} from "./components/user/login/login.component";
+import { NavbarComponent } from './components/shered/navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    NoticiasComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
